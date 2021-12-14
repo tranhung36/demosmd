d7bd0ec699cf95865eca704d8c123052caa5f689
Tran Hung <tranqhung36@gmail.com>
2021-12-14 15:08:51 +0700

    update pagination

--- js/main.js
+++ js/main.js
@@ -1,30 +1,36 @@
-var productsApi = 'https://fakestoreapi.com/products';
+var productsApi = `https://fakestoreapi.com/products`;
 
 fetch(productsApi)
-    .then(res => res.json())
-    .then((products) => {
-        var htmls = products.map((product) => {
-            return `<div class="product text-center col-lg-3 col-md-4 col-12">
-                <img src="${product.image}" alt="" class="img-fluid mb-3">
-                <div class="star">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                </div>
-                <h5 class="product-name">
-                    ${product.title}
-                </h5>
-                <h4 class="product-price">
-                    $${product.price}
-                </h4>
-                <button class="buy-btn">Buy Now</button>
-            </div>`
+  .then((response) => response.json())
+  .then((data) => {
+    $("#list").pagination({
+      dataSource: data,
+      pageSize: 8,
+      callback: (data, pagination) => {
+        var newProducts = data.map((product) => {
+          return `<div class="product text-center col-lg-3 col-md-4 col-12">
+                    <img src="${product.image}" alt="" class="img-fluid mb-3">
+                    <div class="star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h5 class="product-name">
+                        ${product.title}
+                    </h5>
+                    <h4 class="product-price">
+                        $${product.price}
+                    </h4>
+                    <button class="buy-btn">Buy Now</button>
+                </div>`;
         });
-
-        var html = htmls.join('');
-        document.getElementById('products').innerHTML = html;
-    })
-    .then(json => console.log(json))
-
+        const html = newProducts.join("");
+        document.getElementById("products").innerHTML = html;
+      },
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
